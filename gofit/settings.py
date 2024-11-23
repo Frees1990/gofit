7fcb513b61cff628e87a7f0d2738efee8b2d418a
Frees1990 <fairmejor@gmail.com>
2024-11-23 19:56:07 +0000

    Removed secret key and set debug

--- gofit/settings.py
+++ gofit/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0*)w@u-%)+^bv!a$3=&79tlj%!lr$6e@qc@q67kp0p@5--+&#6'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gofit-d18fb7690928.herokuapp.com']
 
