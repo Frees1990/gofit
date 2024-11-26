f5c7b0a871522ce338dd2df3b4e32b4017cdb50a
Frees1990 <fairmejor@gmail.com>
2024-11-26 11:45:44 +0000

    Added toast alerts

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 
+from products.models import Product
+
 # Create your views here.
 
 def view_bag(request):
@@ -11,6 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -19,6 +22,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -26,21 +30,18 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
-    try:
-        quantity = int(request.POST.get('quantity', 0))
-        bag = request.session.get('bag', {})  # Example: Retrieving a bag from the session
+    quantity = int(request.POST.get('quantity', 0))
+    bag = request.session.get('bag', {})  # Example: Retrieving a bag from the session
 
-        if quantity > 0:
-            bag[item_id] = quantity
-        else:
-            bag.pop(item_id, None)
+    if quantity > 0:
+        bag[item_id] = quantity
+        
+    else:
+        bag.pop(item_id, None)
 
         request.session['bag'] = bag  # Update the session
-        messages.success(request, "Bag updated!")
         return redirect('view_bag')  # Adjust the redirect accordingly
-    except Exception as e:
-        messages.error(request, "There was an error updating the bag.")
-        return redirect('view_bag')  # Adjust as necessary
+        
 
 def remove_from_bag(request, item_id):
     """Remove a product from the bag."""
@@ -56,8 +57,6 @@ def remove_from_bag(request, item_id):
 
         # Update the session with the modified bag
         request.session['bag'] = bag
-        messages.success(request, "Item removed from bag!")
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, f"Error removing item: {e}")
         return HttpResponse(status=500)
