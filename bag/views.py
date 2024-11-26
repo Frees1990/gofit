ed869a5d170779148ce809687a83f51483727cea
Frees1990 <fairmejor@gmail.com>
2024-11-26 11:58:53 +0000

    Toasts is not working trying to find a solution saving my changes not to lose it

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -20,6 +20,7 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} Quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
@@ -30,33 +31,41 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity', 0))
     bag = request.session.get('bag', {})  # Example: Retrieving a bag from the session
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         
     else:
         bag.pop(item_id, None)
+        messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag  # Update the session
         return redirect('view_bag')  # Adjust the redirect accordingly
-        
+
 
 def remove_from_bag(request, item_id):
     """Remove a product from the bag."""
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
 
         if item_id in bag:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
         # Update the session with the modified bag
         request.session['bag'] = bag
         return HttpResponse(status=200)
+
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
