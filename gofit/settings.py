83b50d7577c4b82e3fcde7b3efb53fc5ef1d72e2
Frees1990 <fairmejor@gmail.com>
2024-11-26 14:15:13 +0000

    created crispy forms

--- gofit/settings.py
+++ gofit/settings.py
@@ -51,7 +51,9 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
 
+
     #others
+    'crispy_forms',
     'storages',
 ]
 
@@ -66,6 +68,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'gofit.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -84,6 +88,10 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
