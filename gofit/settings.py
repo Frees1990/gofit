9ebc443ebce1354ff164b9d1c4e6b46d39344e74
Frees1990 <fairmejor@gmail.com>
2024-11-27 11:24:47 +0000

    Created profile app

--- gofit/settings.py
+++ gofit/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'gallery',
     'bag',
     'checkout',
+    'profiles',
 
 
     #others
--- gofit/urls.py
+++ gofit/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('gallery/', include('gallery.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
